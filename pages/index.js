a83783e21724764581770a527498279e2a8cf3d0
Gabriel Luiz da Silva <gabrielluiz3011@gmail.com>
2021-03-26 18:25:53 -0300

    second commid

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,48 @@
+import React from 'react';
 import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 import db from '../db.json';
 import Widget from '../src/components/Widget';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
-
-const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
+import QuizLogo from '../src/components/QuizLogo';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
+import QuizContainer from '../src/components/QuizContainer';
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
-    
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Dragon QuiZ</title>
+      </Head>
       <QuizContainer>
+        <QuizLogo />
         <Widget>
           <Widget.Header>
             <h1>Dragon QuiZ</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>teste teste teste</p>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+              <Input
+                name="nomeDoUsuario"
+                onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
+                placeholder="Escreva seu nome"
+                value={name}
+              />
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}
+              </Button>
+            </form>
           </Widget.Content>
         </Widget>
         <Widget>
@@ -37,12 +53,10 @@ export default function Home() {
             <p>teste teste teste</p>
           </Widget.Content>
         </Widget>
-        
         <Footer />
       </QuizContainer>
-        <GitHubCorner />
+      <GitHubCorner projectUrl="https://github.com/gabrielluiz98/aluraquiz-dragonquiz" />
 
     </QuizBackground>
-  
-  )
+  );
 }
